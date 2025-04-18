0dd0f2b44f546e946527820a1764befe22815c08
Mohamed-Essam-Mohamed <152906492+Mohamed-Essam-Mohamed@users.noreply.github.com>
2025-04-18 16:43:32 +0200

    finished app java

--- comprehensive-student-analyti/src/main/java/com/norhan/Application.java
+++ comprehensive-student-analyti/src/main/java/com/norhan/Application.java
@@ -13,6 +13,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
